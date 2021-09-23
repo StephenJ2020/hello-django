42d6c794db4638d83e13ac4030c3ad3313e866ec
Stephen <sjohnston.ie@gmail.com>
2021-09-23 19:20:14 +0000

    feat: Replace say_hello function with get_todo_list functions

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+# def say_hello(request):
+    # return HttpResponse("Hello!") #Change to use render now that we hav e created todo_list.html template, see new function get_todo_list below
+
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
