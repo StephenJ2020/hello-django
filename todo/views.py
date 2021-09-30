daf123579d70bd2b62cd91df66cb0ec84e3d09a0
Stephen <sjohnston.ie@gmail.com>
2021-09-30 17:20:21 +0000

    feat: Add items for loop to todo_list.html template

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from .models import Items
 
 # Create your views here.
 # def say_hello(request):
-    # return HttpResponse("Hello!") #Change to use render now that we hav e created todo_list.html template, see new function get_todo_list below
+# return HttpResponse("Hello!") #Change to use render now that we have
+# created todo_list.html template, see new function get_todo_list below
+
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Items.objects.all()
+    context = {
+        "items": items
+    }
+    return render(request, 'todo/todo_list.html', context)
