acaa7c88ee9ae8b76779900a4db0a2813936813e
Stephen <sjohnston.ie@gmail.com>
2021-10-10 00:23:26 +0000

    feat: Create test files and run basic test in tests_views.py

--- todo/views.py
+++ todo/views.py
@@ -63,7 +63,7 @@ def toggle_item(request, item_id):
     return redirect('get_todo_list')
 
 
-    def delete_item(request, item_id):
+def delete_item(request, item_id):
     item = get_object_or_404(Items, id=item_id)
     item.delete()
     return redirect('get_todo_list')
